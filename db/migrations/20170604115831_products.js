4845df80be2ad8d4149b775ba0027b57d64a1f31
Betty Chempananical <bettymariam@gmail.com>
2017-06-04 12:38:14 -0700

    update products table with mirgation

--- db/migrations/20170604115831_products.js
+++ db/migrations/20170604115831_products.js
@@ -1,8 +1,15 @@
+exports.up = (knex => {
+  return knex.schema.createTable('products', (table) => {
+    table.increments();
+    table.string('name').notNullable();
+    table.integer('category_id').notNullable();
+    table.decimal('price').notNullable();
+    table.text('description').notNullable();
+    table.integer('review_id').notNullable();
+    table.timestamps(true, true);
+  });
+});
 
-exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = (knex => {
+  return knex.schema.dropTable('products');
+});
