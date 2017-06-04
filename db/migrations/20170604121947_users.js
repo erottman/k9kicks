05e1a0f088c5c56da98f039c15e0fc70cf28c028
Betty Chempananical <bettymariam@gmail.com>
2017-06-04 14:43:26 -0700

    Changed migrations and added seed files

--- db/migrations/20170604121947_users.js
+++ db/migrations/20170604121947_users.js
@@ -1,7 +1,6 @@
 exports.up = (knex => {
   return knex.schema.createTable('users', (table) => {
     table.increments();
-    table.integer('customer_id').notNullable();
     table.string('username').notNullable();
     table.string('password').notNullable();
     table.string('role').notNullable();
--- db/migrations/20170604121953_customers.js
+++ db/migrations/20170604121953_customers.js
@@ -1,6 +1,7 @@
 exports.up = (knex => {
   return knex.schema.createTable('customers', (table) => {
     table.increments();
+    table.integer('user_id').notNullable();
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
     table.string('address_line1').notNullable();
