b4daeb52e6ff3299c5258a50dcbf32d0cdc56e65
Betty Chempananical <bettymariam@gmail.com>
2017-06-04 15:07:41 -0700

    Added seeds and changed migrations for users and customers table

--- db/seeds/4_users.js
+++ db/seeds/4_users.js
@@ -1,1 +1,33 @@
 
+exports.seed = (knex) => {
+  return knex('users').del()
+  .then(() => {
+    return knex('users').insert([{
+        id:1,
+        username: 'admin',
+        password: 'admin',
+        role: 'admin'
+      }, {
+        id:2,
+        username: 'betty',
+        password: 'betty',
+        role: 'admin'
+      }, {
+        id:3,
+        username: 'erica',
+        password: 'erica',
+        role: 'admin'
+      },{
+        id:4,
+        username: 'amber',
+        password: 'amber',
+        role: 'admin'
+      }
+    ]);
+  })
+  .then(() => {
+    return knex.raw(
+      "SELECT setval('promotions_id_seq', (SELECT MAX(id) FROM promotions));"
+    );
+  });
+};
