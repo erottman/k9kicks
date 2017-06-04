38ff52499c976e104061235e387608fe4e6b0ae1
Betty Chempananical <bettymariam@gmail.com>
2017-06-04 14:27:07 -0700

    Added seeds

---
+++ db/seeds/2_promotions.js
@@ -0,0 +1,32 @@
+
+exports.seed = (knex) => {
+  return knex('promotions').del()
+  .then(() => {
+    return knex('promotions').insert([{
+      id:1,
+      name: 'Seasonal',
+      rate: 20
+      }, {
+      id:2,
+      name: 'Clearance',
+      rate: 50
+      }, {
+      id:3,
+      name: 'Holiday',
+      rate: 25
+      },{
+      id:4,
+      name: 'Military',
+      rate: 10
+      },{
+      id:5,
+      name: 'None',
+      rate: 0
+    }]);
+  })
+  .then(() => {
+    return knex.raw(
+      "SELECT setval('promotions_id_seq', (SELECT MAX(id) FROM promotions));"
+    );
+  });
+};
